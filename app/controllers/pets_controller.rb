ec26fd80d26c4a6723d292f4d53d57bb57601cbe
irisfaraway <iris.faraway@gmail.com>
2016-06-25 19:46:11 +0100

    update allowed params

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -94,7 +94,6 @@ class PetsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through
   def pet_params
-    params.require(:pet).permit(:name,
-                                :last_fed)
+    params.require(:pet).permit(:name)
   end
 end
