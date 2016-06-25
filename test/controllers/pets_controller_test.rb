1de860f029f348c35cf23055e17f0dbba4146983
irisfaraway <iris.faraway@gmail.com>
2016-06-25 23:16:01 +0100

    improve tests for pets and authorisation

--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -3,11 +3,18 @@ require 'test_helper'
 # Tests for pet class
 class PetsControllerTest < ActionController::TestCase
   setup do
+    # Set pets
     @show_pet = pets(:show_pet)
     @edit_pet = pets(:edit_pet)
     @update_pet = pets(:update_pet)
     @destroy_pet = pets(:destroy_pet)
     @feed_pet = pets(:feed_pet)
+    @normal_user_pet = pets(:normal_user_pet)
+
+    # Set user and session
+    @admin_user = users(:admin_user)
+    @normal_user_one = users(:normal_user_one)
+    session[:user_id] = @admin_user.id
   end
 
   test 'should get index' do
@@ -17,6 +24,7 @@ class PetsControllerTest < ActionController::TestCase
   end
 
   test 'should get new' do
+    session[:user_id] = @normal_user_one.id
     get :new
     assert_response :success
   end
@@ -48,7 +56,6 @@ class PetsControllerTest < ActionController::TestCase
     assert_difference('Pet.count', -1) do
       delete :destroy, id: @destroy_pet
     end
-
     assert_redirected_to pets_path
   end
 
@@ -56,5 +63,17 @@ class PetsControllerTest < ActionController::TestCase
     get :feed, id: @feed_pet
     assert_redirected_to pet_path(assigns(:pet))
     assert_operator @feed_pet.reload.hunger, :<, 1
+  end
+
+  test 'should let admins feed pets that are not theirs' do
+    get :feed, id: @normal_user_pet
+    assert_redirected_to pet_path(assigns(:pet))
+    assert_operator @normal_user_pet.reload.hunger, :<, 1
+  end
+
+  test 'should not let normal users feed pets that are not theirs' do
+    session[:user_id] = @normal_user_one.id
+    get :feed, id: @feed_pet
+    assert_response 302
   end
 end
