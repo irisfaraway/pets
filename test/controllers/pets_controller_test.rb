be0732c985189d940957f262f5bf3690e2f2fb48
irisfaraway <iris.faraway@gmail.com>
2016-06-25 17:50:08 +0100

    update pet tests

--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -1,47 +1,51 @@
 require 'test_helper'
 
+# Tests for pet class
 class PetsControllerTest < ActionController::TestCase
   setup do
-    @pet = pets(:one)
+    @show_pet = pets(:show_pet)
+    @edit_pet = pets(:edit_pet)
+    @update_pet = pets(:update_pet)
+    @destroy_pet = pets(:destroy_pet)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:pets)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should create pet" do
+  test 'should create pet' do
     assert_difference('Pet.count') do
-      post :create, pet: { name: @pet.name }
+      post :create, pet: { name: 'Gavin' }
     end
 
     assert_redirected_to pet_path(assigns(:pet))
   end
 
-  test "should show pet" do
-    get :show, id: @pet
+  test 'should show pet' do
+    get :show, id: @show_pet
     assert_response :success
   end
 
-  test "should get edit" do
-    get :edit, id: @pet
+  test 'should get edit' do
+    get :edit, id: @edit_pet
     assert_response :success
   end
 
-  test "should update pet" do
-    patch :update, id: @pet, pet: { name: @pet.name }
+  test 'should update pet' do
+    patch :update, id: @update_pet, pet: { name: 'Fluffy' }
     assert_redirected_to pet_path(assigns(:pet))
   end
 
-  test "should destroy pet" do
+  test 'should destroy pet' do
     assert_difference('Pet.count', -1) do
-      delete :destroy, id: @pet
+      delete :destroy, id: @destroy_pet
     end
 
     assert_redirected_to pets_path
