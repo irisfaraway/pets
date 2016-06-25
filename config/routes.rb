9511acaa0680088976d1e9a22b81070544cce7e7
irisfaraway <iris.faraway@gmail.com>
2016-06-25 21:04:31 +0100

    configure omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'pets#index'
 
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+
   resources :pets do
     member do
       get 'feed'
